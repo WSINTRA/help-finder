202116b750d276d64be6dd1c56a538374a15d45b
WSINTRA <will.simpson85@gmail.com>
2019-03-26 13:42:35 -0400

    migrate files in place

---
+++ app/controllers/facility_services_controller.rb
@@ -0,0 +1,2 @@
+class FacilityServicesController < ApplicationController
+end
---
+++ test/controllers/facility_services_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FacilityServicesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
