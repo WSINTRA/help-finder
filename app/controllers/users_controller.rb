eee3c0799498876c08c2bd679bb97d0ab6be9ba9
char <25884226+char@users.noreply.github.com>
2019-03-27 08:58:25 -0400

    Fixed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
-  
     @user = User.create(new_params)
-    if user.valid?
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to @user
     end
   end
 
-
   private
+
   def new_params
-  params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
